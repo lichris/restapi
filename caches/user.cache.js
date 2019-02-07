3269ebda3c86aafef4ca375fd8afa64bb87a3b98
Heeseung Lee <final.lee@thecorporation.co.kr>
2019-02-07 22:42:48 +0900

    redis 버그 수정

--- caches/user.cache.js
+++ caches/user.cache.js
@@ -2,70 +2,58 @@ const redis = require('redis')
 const bluebird = require('bluebird')
 import UserRepo from '../repositories/user.repository'
 
+const client = redis.createClient()
+client.on('error', e => {
+  console.error(`redis error : ${e}`)
+})
+
+bluebird.promisifyAll(client)
+
 class UserCache {
-  constructor() {
-    this.client = redis.createClient()
-    this.client.on('connenct', () => {
-      bluebird.promisifyAll(client)
-    })
-
-    this.client.on('error', (e) => {
-      console.error(`redis error : ${e}`)
-    })
-  }
-
   async store(user) {
     try {
-      await this.client.hsetAsync('users:id', [user.id, user.uuid])
-      await this.client.hsetAsync('users:email', [user.email, user.uuid])
-      await this.client.hsetAsync('users:uuid', [user.uuid, JSON.stringify(user.toJSON())])
+      await client.hsetAsync('users:id', [user.id, user.uuid])
+      await client.hsetAsync('users:email', [user.email, user.uuid])
+      await client.hsetAsync('users:uuid', [user.uuid, JSON.stringify(user.toJSON())])
     } catch (e) {
-      // error 로깅 
+      // error 로깅
+      console.error(e)
     }
   }
 
   async find(uuid) {
     if (uuid) {
       try {
-        const user = await this.client.hgetAsync('users:uuid', uuid)
-
-        if (!user) {
-          return null
-        }
-        
-        return JSON.parse(user)
+        return client.hgetAsync('users:uuid', uuid)
       } catch (e) {
         // error 로깅
         return null
       }
     }
-    return null
   }
 
   async findById(id) {
     if (id) {
       try {
-        const uuid = await this.client.hgetAsync('users:id', id)
+        const uuid = await client.hgetAsync('users:id', id)
         return this.find(uuid)
       } catch (e) {
         // error 로깅
         return null
       }
     }
-    return null
   }
 
   async findByEmail(email) {
     if (email) {
       try {
-        const uuid = await this.client.hgetAsync('users:email', email)
+        const uuid = await client.hgetAsync('users:email', email)
         return this.find(uuid)
       } catch (e) {
         // error 로깅
         return null
       }
     }
-    return null
   }
 }
 
